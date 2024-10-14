a9835342e151a93b97d0b445f460763d77cdbebb
Leonardo <lsr_31@hotmail.com>
2024-10-14 11:18:23 -0400

    performance improvements with joblib

--- test.py
+++ test.py
@@ -1,214 +1,58 @@
 import pandas as pd
-from pandasgui import show
-from thefuzz import process, fuzz
-from joblib import Parallel, delayed
-from collections import Counter
-import time
-import re
+import matplotlib.pyplot as plt
+from unidecode import unidecode
 
-#################################################### FUNCTIONS ######################################
-def validate_change_date(df, columns, fill_date, amount_null):
-    """
-    Validate the data filled with the date 1900-01-01 is the same that null data
-    """ 
-    count = (df[columns] == fill_date).sum().sum()
-    if count == amount_null:
-        return 'OK'
-    else:
-        return f"Fill data error: {count} != {amount_null}"
-    
-def transform_to_string(df, value):
-    for obj in value:
-        df[obj] = df[obj].apply(lambda x: ', '.join(sorted(x)))
-    
-def normalize_category_fuzzy(value, choices, threshold=80):
-    """ 
-    Normalize the categories for each categorical data
-    """
-    if isinstance(value, str):
-        best_match, best_score = process.extractOne(value, choices)
-        if best_score >= threshold:
-            return best_match
-    return 'nr'
+clean_data = pd.read_csv('data/rotten_tomatoes_movies_clean.csv')
+rating_perfect = clean_data[(clean_data['tomatometer_rating'] ==100) & (clean_data['audience_rating'] == 100)]
+print(rating_perfect[rating_perfect['tomatometer_status'] == 'fresh'])
 
-def fill_date_null(df, dates, date_fict):
-    """
-    fill the null dates with fictitious dates: 1900-01-01
-    """
-    for col in dates:
-        df[col] = pd.to_datetime(df[col], errors='coerce')
-        df[col] = df[col].fillna(pd.Timestamp(date_fict))
-    return df[col]
+# Descomponer la cadena de generos en una lista
+df_normalized = clean_data[clean_data['directors'] != 'nr'].copy()
 
-# Compilar la expresión regular fuera del bucle para evitar compilarla en cada llamada
-irrelevant_terms = re.compile(r'\b(inc|corp|corporation|limited|ltd|plc|pictures|picture|co|entertainment|films|film|studios|corporat)\b')
+df_normalized['genres_normalized'] = df_normalized['genres'].str.split(',') 
 
-# Preprocesamiento de compañías para eliminar términos irrelevantes
-def preprocess_company(company):
-    company = company.lower().strip()               # Convertir a minúsculas y eliminar espacios
-    company = irrelevant_terms.sub('', company)     # Remover términos comunes que no afectan el nombre principal
-    company = re.sub(r'[^\w\s]', '', company)       # Eliminar caracteres especiales
-    company = re.sub(r'\s+', ' ', company).strip()  # Reemplazar múltiples espacios por uno solo
-    return company
+# Normalized genres
+df_normalized['genres_normalized'] = df_normalized['genres_normalized'].apply(lambda x: [unidecode(genre.strip()) for genre in x])
 
-# Función para agrupar compañías similares
-def group_similar(company, all_companies, threshold=85):
-    """
-    Usa extractBests para obtener las mejores coincidencias y agrupar compañías
-    """
-    # Obtener coincidencias
-    matches = process.extractBests(company, all_companies, scorer=fuzz.token_sort_ratio, score_cutoff=threshold)
-    
-    if matches:
-        # Validar que las coincidencias tienen suficiente información antes de intentar acceder a ellas
-        try:
-            # Solo procesar si hay más de un elemento en el tuple
-            match_names = [match[0] for match in matches]  # Obtenemos los nombres directamente
-            most_common = Counter(match_names).most_common(1)
-            return most_common[0][0] if most_common else company  # Devuelve la coincidencia más común
-        except IndexError:
-            return "ERROR"
-    return company  # Devuelve la compañía original si no hay coincidencias
+# Descomponer la cadena directores en una lista
+
+df_normalized['directors_normalized'] = df_normalized['directors'].str.split(',')   
+
+# Normalized directors
+df_normalized['directors_normalized'] = df_normalized['directors_normalized'].apply(lambda x: [unidecode(director.strip()) for director in x])
+
+# Explotar la lista de géneros
+genres = df_normalized['genres_normalized'].explode().str.strip()  # Descompone la lista de géneros
+
+# Explotar la lista de directores
+directors = df_normalized['directors_normalized'].explode().str.strip().str.title()
+
+# Contar la frecuencia de cada género
+genre_counts = genres.value_counts()
+# Contar la frecuencia de cada director
+top_directors = directors.value_counts().nlargest(15)
+
+# # Graficar los 10 directores con mejor calificación promedio
+# plt.figure(figsize=(10,6))
+# plt.barh(top_directors.index, top_directors.values, color='purple', alpha=0.7)
+# plt.xlabel('Number of movies')
+# plt.ylabel('Director')
+# plt.title('Top 15 Directors with the Most Movies Directed')
+# plt.gca().invert_yaxis()  # Invertir el eje Y para mostrar el mejor director en la parte superior
+# plt.tight_layout()
+# plt.show()
+# top_15 = df_normalized.explode('genres_normalized').groupby('genres_normalized')['audience_rating'].median().nlargest(15)
+# top_15.plot(kind='barh', color='red', alpha=0.7)
+# plt.title('Top 15 Géneros con Mejor Audience Rating')
+# plt.xlabel('Género')
+# plt.ylabel('Median Audience Rating')
+# plt.xticks(rotation=45, ha='right')
+# plt.tight_layout()
+# plt.gca().invert_yaxis() 
+# plt.show()
 
 
-# Función para procesar compañías en paralelo
-def process_company(companies,threshold=85):
-    """
-    Usar Parallel para ejecutar la función en paralelo
-    """
-    return Parallel(n_jobs=-1)(
-        delayed(group_similar)(company, companies, threshold) for company in companies
-    )
 
-# Función para dividir en chunks dinámicos
-def chunkify_dynamic(lst, chunk_size):
-    """Divide la lista en chunks de tamaño 'chunk_size'."""
-    return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
-
-# Función para procesar en chunks
-def process_company_in_chunks_dynamic(companies, threshold=85):
-    """
-    Procesar la lista de compañías en chunks más pequeños para optimizar el rendimiento
-    """
-    # Definir tamaño del chunk basado en el tamaño de la lista
-    chunk_size = max(len(companies) // 4000, 1)
-    chunks = chunkify_dynamic(companies, chunk_size)
-    
-    # Procesamos cada chunk en paralelo
-    results = Parallel(n_jobs=-1)(
-        delayed(process_company)(chunk, companies, threshold) for chunk in chunks
-    )
-    
-    # Combinamos los resultados
-    return [company for chunk_result in results for company in chunk_result]
-
-
-############################################### END FUNCTIONS #################################################
-#empezamos con la limpieza
-# 1: Cargamos el dataset con pandas
-raw_data = pd.read_csv('data/rotten_tomatoes_movies.csv')
-amount_dirty_data = raw_data.shape
-
-# 2: Armamos listas segun el tipo de dato
-categorical = ['content_rating','tomatometer_status','audience_status']
-irrelevant = ['rotten_tomatoes_link', 'movie_info', 'critics_consensus','tomatometer_count', 
-    'tomatometer_top_critics_count', 'tomatometer_fresh_critics_count', 'tomatometer_rotten_critics_count','audience_count']
-relevant = ['movie_title']
-objs = ['directors', 'genres', 'authors', 'actors', 'production_company']
-numeric = ['runtime']
-ratings = ['tomatometer_rating', 'audience_rating']
-dates = ['original_release_date', 'streaming_release_date']
-
-# 2.1: definición de categorias
-rating = ['g', 'nc17', 'nr', 'pg', 'pg13', 'r', 'unknown']
-t_status = ['rotten', 'fresh', 'certified-fresh', 'unknown']
-a_status = ['spilled', 'upright', 'unknown']
-
-# 3: Eliminamos columnas irrelevantes para este proceso
-raw_data = raw_data.drop(columns= irrelevant, errors='ignore')
-
-# 4: Revisamos datos nulos en el df y sus tipos
-amount_dirty_nulls = raw_data.isnull().sum()
-type_dirty_data = raw_data.dtypes
-amount_companies = raw_data['production_company'].nunique()
-
-# 5: Eliminamos las filas que tengan datos nulos en las columnas relevantes
-raw_data = raw_data.dropna(subset=relevant)
-
-# Eliminamos las filas duplicadas
-raw_data = raw_data.drop_duplicates(subset=['movie_title','original_release_date','directors'])
-
-# 6: lower
-lower = raw_data.select_dtypes(include=['object','category']).columns
-for col in lower:
-    raw_data[col] = raw_data[col].str.lower()
-    
-# 7: llenamos los datos categoricos nulos
-raw_data[categorical] = raw_data[categorical].fillna('nr').astype('category')
-
-# 8: llenamos los datos objetc nulos
-raw_data[objs] = raw_data[objs].fillna('nr')
-
-# 10: llenamos datos numericos nulos
-raw_data[numeric] = raw_data[numeric].fillna(0.0)
-
-# 9: normalizamos los datos de tipo objeto, se remplaza & con , y elimina espacio vacios
-
-#raw_data[objs] = raw_data[objs].replace('&', ',', regex=True).apply(lambda col: col.str.split(',')).apply(lambda col: col.apply(lambda x: frozenset(map(str.strip, x))))
-raw_data[objs] = (
-    raw_data[objs]
-    .replace('&', ',', regex=True)
-    .apply(lambda col: col.str.split(',').apply(lambda x: frozenset(map(str.strip, x))))
-)
-
-# 11: llenamos los datos rating nulos (Rellena nulos de tomatometer_rating con audience_rating y viceversa)
-raw_data[ratings] = raw_data[ratings].apply(lambda x: x.fillna(raw_data[ratings].mean(axis=1)))
-# rellena nulos en ambas columnas
-raw_data[ratings] = raw_data.groupby('genres')[ratings].transform(lambda x: x.fillna(x.median()))
-# rellena los nulos restantes, si los hay, con la media general
-raw_data[ratings] = raw_data[ratings].fillna(raw_data[ratings].mean())
-
-# 12: llenamos date nulos con 1900-01-01
-fill_date_null(raw_data, dates, '1900-01-01')
-
-# 13: Transformamos los frozenset a cadena
-transform_to_string(raw_data,objs)
-
-# 14: Normalizar categorias
-category_dict = {
-    'content_rating': rating,
-    'tomatometer_status': t_status,
-    'audience_status': a_status
-    }
-for col, choices in category_dict.items():
-    raw_data[col] = raw_data[col].apply(lambda x: normalize_category_fuzzy(x, choices))
-    
-# 15: normalizar productoras
-start_time = time.time()
-
-raw_data['production_company'] = raw_data['production_company'].apply(preprocess_company)
-companies_list = raw_data['production_company'].tolist()
-raw_data['production_company'] = process_company(companies_list)
-
-#show(raw_data.head(200))
-amount_clean_data = raw_data.shape
-amount_clean_nulls = raw_data.isnull().sum()
-type_clean_data = raw_data.dtypes
-
-# print("DIRTY DATA ")
-# print(f"Cantidad de filas inicial: {amount_dirty_data[0]}")
-# print(f"Cantidad de columnas inicial: {amount_dirty_data[1]}\n")
-# print(f"Datos nulos por columna: \n{amount_dirty_nulls} \n")
-# print(f"Tipo de datos por columna: \n{type_dirty_data} \n")
-print(f"Cantidad de compañias: {amount_companies}")
-# print("CLEAN DATA")
-# print(f"Cantidad de filas: {amount_clean_data[0]}")
-# print(f"Cantidad de columnas: {amount_clean_data[1]}\n")
-# print(f"Datos nulos por columna: \n{amount_clean_nulls} \n")
-# print(f"Tipo de datos por columna: \n{type_clean_data}\n")
-print(f"Cantidad de compañias: {raw_data['production_company'].nunique()}" )
-# # Imprimir tiempo total
-print(f"Tiempo de procesamiento: {time.time() - start_time} segundos")
 
 
 
--- transform.py
+++ transform.py
@@ -5,7 +5,10 @@ from joblib import Parallel, delayed
 from collections import Counter
 import time
 import re
-
+from unidecode import unidecode
+from colorama import Style,Fore, init
+init()
+# https://deepnote.com/app/film-data-management-project/Rating-movies-data-cleaning-9fedf1da-d519-4e3a-9f03-5cc227c7f4f5
 #################################################### FUNCTIONS ######################################
 def validate_change_date(df, columns, fill_date, amount_null):
     """
@@ -18,62 +21,71 @@ def validate_change_date(df, columns, fill_date, amount_null):
         return f"Fill data error: {count} != {amount_null}"
     
 def transform_to_string(df, value):
+    """
+    Transform type of data in a column to string.
+    This function assumes that each cell in the specified columns contains an iterable (e.g., a list).
+    It sorts the elements in each iterable and joins them into a single string, separated by commas.
+    """
     for obj in value:
         df[obj] = df[obj].apply(lambda x: ', '.join(sorted(x)))
     
 def normalize_category_fuzzy(value, choices, threshold=80):
     """ 
-    Normalizar las categorías usando rapidfuzz
+    Normalizes a given string value by finding the closest match in a list of choices 
+    using fuzzy matching with rapidfuzz.
     """
     if isinstance(value, str):
-        # Obtener el mejor match y su score
-        best_match = process.extractOne(value, choices)
-        if best_match and best_match[1] >= threshold:  # Verificamos que best_match existe y el score es suficiente
-            return best_match[0]  # Devolver el nombre de la mejor coincidencia
-    return 'nr'  # Devolver 'nr' si no hay coincidencia adecuada
+        best_match = process.extractOne(value, choices)     # get the best match and its score
+        if best_match and best_match[1] >= threshold:       # Ensure the score is sufficient
+            return best_match[0]                            # Return the name of the best match
+    return 'nr'                                             # Return 'nr' if no suitable match was found
 
 def fill_date_null(df, dates, date_fict):
     """
-    fill the null dates with fictitious dates: 1900-01-01
+    Fill null dates in specified columns of a DataFrame with a fictitious date
     """ 
     for col in dates:
         df[col] = pd.to_datetime(df[col], errors='coerce')
         df[col] = df[col].fillna(pd.Timestamp(date_fict))
     return df[col]
 
-# Compilar la expresión regular para eliminar términos irrelevantes
+# Compile the regular expression to delete irrelevants terms
 irrelevant_terms = re.compile(r'\b(inc|corp|corporation|limited|ltd|plc|pictures|picture|co|entertainment|films|film|studios|corporat)\b')
 
 def preprocess_company(company):
-    company = company.lower().strip()               # Convertir a minúsculas y eliminar espacios
-    company = irrelevant_terms.sub('', company)     # Remover términos comunes que no afectan el nombre principal
-    company = re.sub(r'[^\w\s]', '', company)       # Eliminar caracteres especiales
-    company = re.sub(r'\s+', ' ', company).strip()  # Reemplazar múltiples espacios por uno solo
+    """ 
+    Preprocesses a company name by applying several cleaning steps.
+    
+    The function converts the company name to lowercase, removes irrelevant terms, 
+    strips special characters, and normalizes spaces.
+    """
+    company = company.lower().strip()               # Convert the company name to lowercase and remove spaces
+    company = irrelevant_terms.sub('', company)     # Remove irrelevant terms that do not affect the main name
+    company = re.sub(r'[^\w\s]', '', company)       # Remove special characters from the company name
+    company = re.sub(r'\s+', ' ', company).strip()  # Replace multiple spaces with a single space and strip spaces
     return company
 
-# Función para agrupar compañías similares
 def group_similar(company, all_companies, threshold=85):
     """
-    Usa extract para obtener las mejores coincidencias y agrupar compañías.
+    Finding the best matches from a list of company names using fuzzy matching.
     """
-    # Obtener coincidencias con process.extract de rapidfuzz
+    # Find matches with process.extract from RapidFuzz
     matches = process.extract(company, all_companies, scorer=fuzz.token_sort_ratio, score_cutoff=threshold)
     
     if matches:
-        # Validar que las coincidencias tienen suficiente información antes de intentar acceder a ellas
         try:
-            # Solo procesar si hay más de un elemento en el tuple
-            match_names = [match[0] for match in matches]  # Obtenemos los nombres directamente
+            # Extract the matched company names from the tuples
+            match_names = [match[0] for match in matches]  
             most_common = Counter(match_names).most_common(1)
-            return most_common[0][0] if most_common else company  # Devuelve la coincidencia más común
+            return most_common[0][0] if most_common else company   # Return the most common match
         except IndexError:
             return "ERROR"
-    return company  # Devuelve la compañía original si no hay coincidencias
+    return company  # Return the original company name if no matches are found
 
 # Función para procesar compañías en paralelo
 def process_company(companies, threshold=85):
     """
-    Usar Parallel para ejecutar la función en paralelo
+    Processes a list of company names in parallel to group similar companies, applying the group_similar function
     """
     return Parallel(n_jobs=-1)(
         delayed(group_similar)(company, companies, threshold) for company in companies
@@ -81,11 +93,12 @@ def process_company(companies, threshold=85):
 
 ############################################### END FUNCTIONS #################################################
 
-# 1: Cargamos el dataset con pandas
-raw_data = pd.read_csv('data/rotten_tomatoes_movies.csv')
+# 1: Read dataset with pandas
+original_data = pd.read_csv('data/rotten_tomatoes_movies.csv')
+raw_data = original_data.copy()
 amount_dirty_data = raw_data.shape
 
-# 2: Armamos listas segun el tipo de dato
+# 2: Definition of data types
 categorical = ['content_rating','tomatometer_status','audience_status']
 irrelevant = ['rotten_tomatoes_link', 'movie_info', 'critics_consensus','tomatometer_count', 
     'tomatometer_top_critics_count', 'tomatometer_fresh_critics_count', 'tomatometer_rotten_critics_count','audience_count']
@@ -95,62 +108,63 @@ numeric = ['runtime']
 ratings = ['tomatometer_rating', 'audience_rating']
 dates = ['original_release_date', 'streaming_release_date']
 
-# 2.1: definición de categorias
-rating = ['g', 'nc17', 'nr', 'pg', 'pg13', 'r', 'unknown']
-t_status = ['rotten', 'fresh', 'certified-fresh', 'unknown']
-a_status = ['spilled', 'upright', 'unknown']
+# 2.1: Definition of categories
+rating = ['g', 'nc17', 'nr', 'pg', 'pg13', 'r']
+t_status = ['rotten', 'fresh', 'certified-fresh', 'nr']
+a_status = ['spilled', 'upright', 'nr']
 
-# 3: Eliminamos columnas irrelevantes para este proceso
-raw_data = raw_data.drop(columns= irrelevant, errors='ignore')
-
-# 4: Revisamos datos nulos en el df y sus tipos
-amount_dirty_nulls = raw_data.isnull().sum()
+# 3: Revisamos datos nulos en el df y sus tipos
+amount_dirty_nulls = raw_data.isnull().sum().sum()
 type_dirty_data = raw_data.dtypes
 amount_companies = raw_data['production_company'].nunique()
 
-# 5: Eliminamos las filas que tengan datos nulos en las columnas relevantes
+# 4: remove irrelevant columns
+raw_data = raw_data.drop(columns= irrelevant, errors='ignore')
+
+# 4.1: remove rows with missing values in relevant columns
 raw_data = raw_data.dropna(subset=relevant)
 
-# Eliminamos las filas duplicadas
+# 4.2 remove duplicates
 raw_data = raw_data.drop_duplicates(subset=['movie_title','original_release_date','directors'])
 
-# 6: lower
+# 5: fill missing category values
+for col in categorical:
+    raw_data[col] = raw_data[col].str.lower().fillna('nr').astype('category')
+    
+# 5.1: fill missing objects values
+raw_data[objs] = raw_data[objs].fillna('nr')
+
+# 5.2: fill missing numerica values
+raw_data[numeric] = raw_data[numeric].fillna(0.0)
+
+# 5.3: fill missing data vulues 
+amount_date_nulls = raw_data[dates].isnull().sum().sum()
+fill_date_null(raw_data, dates, '1900-01-01')
+validate_change_date(raw_data,dates,'1900-01-01',amount_date_nulls)
+
+# 6: Normalize object columns
 lower = raw_data.select_dtypes(include=['object','category']).columns
 for col in lower:
     raw_data[col] = raw_data[col].str.lower()
     
-# 7: llenamos los datos categoricos nulos
-raw_data[categorical] = raw_data[categorical].fillna('nr').astype('category')
-
-# 8: llenamos los datos objetc nulos
-raw_data[objs] = raw_data[objs].fillna('nr')
-
-# 10: llenamos datos numericos nulos
-raw_data[numeric] = raw_data[numeric].fillna(0.0)
-
-# 9: normalizamos los datos de tipo objeto, se remplaza & con , y elimina espacio vacios
-
-#raw_data[objs] = raw_data[objs].replace('&', ',', regex=True).apply(lambda col: col.str.split(',')).apply(lambda col: col.apply(lambda x: frozenset(map(str.strip, x))))
+# 6.1: Normalize objects with frozenset
 raw_data[objs] = (
     raw_data[objs]
     .replace('&', ',', regex=True)
     .apply(lambda col: col.str.split(',').apply(lambda x: frozenset(map(str.strip, x))))
-)
+)   
 
-# 11: llenamos los datos rating nulos (Rellena nulos de tomatometer_rating con audience_rating y viceversa)
+# 7: Fill nulls of tomatometer_rating with audience_rating and vice versa
 raw_data[ratings] = raw_data[ratings].apply(lambda x: x.fillna(raw_data[ratings].mean(axis=1)))
-# rellena nulos en ambas columnas
+# 7.2: Fill nulls in both columns with median
 raw_data[ratings] = raw_data.groupby('genres')[ratings].transform(lambda x: x.fillna(x.median()))
 # rellena los nulos restantes, si los hay, con la media general
 raw_data[ratings] = raw_data[ratings].fillna(raw_data[ratings].mean())
 
-# 12: llenamos date nulos con 1900-01-01
-fill_date_null(raw_data, dates, '1900-01-01')
-
-# 13: Transformamos los frozenset a cadena
+# 8: Transfor frozenset to string
 transform_to_string(raw_data,objs)
 
-# 14: Normalizar categorias
+# 9: Normalize categories
 category_dict = {
     'content_rating': rating,
     'tomatometer_status': t_status,
@@ -159,33 +173,69 @@ category_dict = {
 for col, choices in category_dict.items():
     raw_data[col] = raw_data[col].apply(lambda x: normalize_category_fuzzy(x, choices)).astype('category')
     
-# 15: normalizar productoras
+# 9.1: Normalize companies with rapidfuzz
 start_time = time.time()
 
 raw_data['production_company'] = raw_data['production_company'].apply(preprocess_company)
 companies_list = raw_data['production_company'].tolist()
 raw_data['production_company'] = process_company(companies_list, threshold=85)
 
-show(raw_data.head(200))
-amount_clean_data = raw_data.shape
-amount_clean_nulls = raw_data.isnull().sum()
-type_clean_data = raw_data.dtypes
+# 10: define clean_data dataframe
+clean_data = raw_data
 
-# print("DIRTY DATA ")
-# print(f"Cantidad de filas inicial: {amount_dirty_data[0]}")
-# print(f"Cantidad de columnas inicial: {amount_dirty_data[1]}\n")
-# print(f"Datos nulos por columna: \n{amount_dirty_nulls} \n")
-# print(f"Tipo de datos por columna: \n{type_dirty_data} \n")
-print(f"Cantidad de compañias: {amount_companies}")
-# print("CLEAN DATA")
-# print(f"Cantidad de filas: {amount_clean_data[0]}")
-# print(f"Cantidad de columnas: {amount_clean_data[1]}\n")
-# print(f"Datos nulos por columna: \n{amount_clean_nulls} \n")
-# print(f"Tipo de datos por columna: \n{type_clean_data}\n")
-print(f"Cantidad de compañias: {raw_data['production_company'].nunique()}" )
+# 11: load data
+clean_data.to_csv("data/rotten_tomatoes_movies_clean.csv", index = False)
+
+show(clean_data.head(200))
+
+amount_clean_data = clean_data.shape
+amount_clean_nulls = clean_data.isnull().sum().sum()
+type_clean_data = clean_data.dtypes
+
+print(Fore.RED + "DIRTY DATA" + Style.RESET_ALL)
+print(f"Cantidad de filas inicial: {amount_dirty_data[0]}")
+print(f"Cantidad de columnas inicial: {amount_dirty_data[1]}")
+print(f"Cantidad Datos nulos: {amount_dirty_nulls}")
+print(f"Cantidad de compañias: {amount_companies} ")
+print(f"Tipo de datos por columna: \n{type_dirty_data} \n")
+
+print(Fore.GREEN + "CLEAN DATA" + Style.RESET_ALL)
+print(f"Cantidad de filas: {amount_clean_data[0]}")
+print(f"Cantidad de columnas: {amount_clean_data[1]}")
+print(f"cantidad Datos nulos: {amount_clean_nulls} ")
+print(f"Cantidad de compañias: {clean_data['production_company'].nunique()}" )
+print(f"Tipo de datos por columna: \n{type_clean_data}\n")
+
 # # Imprimir tiempo total
 print(f"Tiempo de procesamiento: {time.time() - start_time} segundos")
-print(raw_data.columns)
+
+############################################################################### DESCOMPOSE ################################################### 
+# Decompose the genre string into a list
+df_normalized = clean_data[clean_data['directors'] != 'nr'].copy()
+
+df_normalized['genres_normalized'] = df_normalized['genres'].str.split(',') 
+
+# Normalized genres
+df_normalized['genres_normalized'] = df_normalized['genres_normalized'].apply(lambda x: [unidecode(genre.strip()) for genre in x])
+
+# Descompose the directors string into a list
+
+df_normalized['directors_normalized'] = df_normalized['directors'].str.split(',')   
+
+# Normalized directors
+df_normalized['directors_normalized'] = df_normalized['directors_normalized'].apply(lambda x: [unidecode(director.strip()) for director in x])
+
+# Explode the list of genres
+genres = df_normalized['genres_normalized'].explode().str.strip()  
+
+# Explode the list of directors
+directors = df_normalized['directors_normalized'].explode().str.strip().str.title()
+
+# Count the frecuency of each genre
+genre_counts = genres.value_counts()
+
+# Count the frecuency of each director
+top_directors = directors.value_counts().nlargest(15)
 
 
 
